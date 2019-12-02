ab60baf41e64c2358408889fb33416735a765588
Julius Casipit <52521862+juggernautSmash@users.noreply.github.com>
2019-12-02 01:09:15 -0800

    updated app listen port for heroku deplyment

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express()
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.listen(3000)
 
 require('./app/routes')(app)
+
+app.listen(process.env.PORT || 3000)
